5d6687ed56f09946859dfc4fb0912701315175a3
Josiah Aubin <josiahaubin@gmail.com>
2019-09-01 15:13:02 -0600

    Weather location and style

--- app/models/weather.js
+++ app/models/weather.js
@@ -16,6 +16,7 @@ export default class Weather {
     let tempF = Math.floor(9 / 5 * (this.kelvin - 273) + 32)
     return `
       <h3 class="text-right">${tempF}&degF</h3>
+      <p class="text-right locationWeather">-${this.city}</p>
     `
   }
 }
